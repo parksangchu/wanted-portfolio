9d79b31da2ce8c6c5d3728e0635e263f4e23a774
sangchu <didi1484@gmail.com>
2024-08-01 10:58:19 +0900

    feat: 멤버 이름 중복 검증로직 추가

--- src/main/java/com/wanted/portfolio/member/service/MemberService.java
+++ src/main/java/com/wanted/portfolio/member/service/MemberService.java
@@ -4,6 +4,7 @@ import com.wanted.portfolio.global.exception.BadRequestException;
 import com.wanted.portfolio.global.exception.NotFoundException;
 import com.wanted.portfolio.member.dto.MemberRequest;
 import com.wanted.portfolio.member.model.Member;
+import com.wanted.portfolio.member.model.Role;
 import com.wanted.portfolio.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,10 +23,11 @@ public class MemberService {
     public Member createMember(MemberRequest memberRequest) {
 
         validateDuplicateEmail(memberRequest);
+        validateDuplicateName(memberRequest);
 
         String encodedPassword = passwordEncoder.encode(memberRequest.getPassword());
 
-        Member member = toMember(memberRequest, encodedPassword);
+        Member member = toMember(memberRequest, encodedPassword, Role.USER);
         memberRepository.save(member);
 
         log.info("새로운 멤버가 생성되었습니다. : {}", member);
@@ -44,8 +46,14 @@ public class MemberService {
         }
     }
 
-    private Member toMember(MemberRequest memberRequest, String encodedPassword) {
+    private void validateDuplicateName(MemberRequest memberRequest) {
+        if (memberRepository.existsByName(memberRequest.getName())) {
+            throw new BadRequestException("이미 사용중인 이름입니다.");
+        }
+    }
+
+    private Member toMember(MemberRequest memberRequest, String encodedPassword, Role role) {
         return new Member(memberRequest.getEmail(), memberRequest.getPhoneNumber(), memberRequest.getName(),
-                encodedPassword);
+                encodedPassword, role);
     }
 }
