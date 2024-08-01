8f1230e96dc476a1390304c30a58dbf3ad794082
sangchu <didi1484@gmail.com>
2024-08-01 11:55:49 +0900

    feat: 클라이언트단과 연동을 위한 cors 설정

--- src/main/java/com/wanted/portfolio/config/SecurityConfig.java
+++ src/main/java/com/wanted/portfolio/config/SecurityConfig.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wanted.portfolio.auth.filter.JwtFilter;
 import com.wanted.portfolio.auth.filter.LoginFilter;
 import com.wanted.portfolio.auth.jwt.JwtUtil;
+import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,6 +20,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -30,6 +33,21 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // cors
+
+        http.cors(cors -> cors.configurationSource(request -> {
+
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+            configuration.setAllowedMethods(Collections.singletonList("*"));
+            configuration.setAllowedHeaders(Collections.singletonList("*"));
+            configuration.setAllowCredentials(true);
+            configuration.setMaxAge(3600L);
+
+            configuration.setExposedHeaders(Collections.singletonList(HttpHeaders.AUTHORIZATION));
+
+            return configuration;
+        }));
 
         // 미사용
         http
